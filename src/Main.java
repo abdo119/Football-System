237682fa80e6a5b3b8d81fa005a393a68a5864cd
abdo119 <cairooo119@gmail.com>
2021-06-07 23:49:06 +0200

    football system

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * this program is used to manage Football system
+ *
+ * @author Abdelrahman Ahmed
+ * @author islam Abououf
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        new PlaygroundSystem() ;
+    }
+}
